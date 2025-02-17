4d9e96df180cf8a3a36ecd35a5edf673c8785953
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-02-17 22:19:42 +0100

    Add constructor, getters and toString

--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,23 @@
 public class Author {
     int id;
     String name;
+
+    public Author(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                "\tName: " + name;
+    }
 }
