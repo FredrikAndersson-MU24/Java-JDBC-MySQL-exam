6b59c74b766229a61a8c939077a56a10c6d0d215
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-02-17 21:35:18 +0100

    Prohibit a user to lend a book that is already lent
    Add necessary getters

--- src/BookDAO.java
+++ src/BookDAO.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookDAO {
+    private Connection conn = Database.getConnection();
+
 
     public void addBook(String title, int authorId) {
         String query = "INSERT INTO books (title, author, available) VALUES (?, ?, ?);";
         boolean available = true;
         try {
-            Connection conn = Database.getConnection();
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, title);
             stmt.setInt(2, authorId);
@@ -20,23 +21,36 @@ public class BookDAO {
         }
     }
 
+    public Book getBookById(int bookId) {
+        String query = "SELECT * FROM books WHERE id = ?;";
+        Book book = null;
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, bookId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){
+                book = new Book(rs.getInt(1), rs.getString(2), rs.getInt(3),rs.getBoolean(4));
+            }
+        } catch (SQLException e){
+            System.out.println("Failed to get book by ID!");
+            e.printStackTrace();
+        }
+        return book;
+    }
+
     public List<Book> getAllBooks() {
         String query = "SELECT * FROM books;";
         List<Book> listOfBooks = new ArrayList<>();
-
         try {
-            Connection conn = Database.getConnection();
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 listOfBooks.add(new Book(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getBoolean(4)));
             }
-
         } catch (SQLException e) {
             System.out.println("Failed to get all books!");
             e.printStackTrace();
         }
-
         return listOfBooks;
     }
 
@@ -44,9 +58,7 @@ public class BookDAO {
         List<Book> listOfBooks = getAllBooks();
         listOfBooks.forEach(b -> System.out.println("ID: " + b.id + "Title: " + b.title));
         String query = "DELETE FROM books WHERE id = ?;";
-
         try {
-            Connection conn = Database.getConnection();
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setInt(1, id);
             stmt.executeUpdate();
