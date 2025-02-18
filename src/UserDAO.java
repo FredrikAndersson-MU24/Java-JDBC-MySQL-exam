fe6f4b6fa233478379552b7d2918eae934293e4e
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-02-18 19:20:55 +0100

    Adds getPassword and getUser

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -40,6 +40,54 @@ public class UserDAO {
         return usernames;
     }
 
+    public String getPassword(String username){
+        String query = "SELECT password FROM users WHERE username = ?;";
+        String password = "";
+        try {
+            PreparedStatement getPwd = conn.prepareStatement(query);
+            getPwd.setString(1, username);
+            ResultSet rs = getPwd.executeQuery();
+            if(rs.next()){
+                password = rs.getString(1);
+            }
+        } catch (SQLException e){
+            System.out.println("Failed to get password!");
+            e.printStackTrace();
+        }
+        return password;
+    }
+
+    public User getUser(String username){
+        String query = "SELECT * FROM users WHERE username = ?";
+        User user = null;
+        boolean isAdmin = false;
+        try {
+            PreparedStatement getUser = conn.prepareStatement(query);
+            getUser.setString(1, username);
+            ResultSet rs = getUser.executeQuery();
+            if(rs.next()) {
+                isAdmin = rs.getBoolean(6);
+                if (isAdmin) {
+                    user = new AdminUser(
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4));
+                } else {
+                    user = new RegisteredUser(
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4),
+                            rs.getInt(5));
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to get user!");
+            e.printStackTrace();
+        }
+        return user;
+    }
 
 
 }
