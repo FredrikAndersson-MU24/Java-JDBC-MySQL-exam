6575ad6d57e45530fd049e2229f873ec4b375526
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-02-23 11:02:24 +0100

    Formatting

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -10,11 +10,11 @@ public class UserDAO {
         String query = "INSERT INTO users (name, username, password, loan_period, admin_rights) VALUES (?, ?, ?, ?, ?);";
         try {
             PreparedStatement addUser = conn.prepareStatement(query);
-            addUser.setString(1,name);
-            addUser.setString(2,username);
-            addUser.setString(3,password);
-            addUser.setInt(4,loanPeriod);
-            addUser.setBoolean(5,admin);
+            addUser.setString(1, name);
+            addUser.setString(2, username);
+            addUser.setString(3, password);
+            addUser.setInt(4, loanPeriod);
+            addUser.setBoolean(5, admin);
             addUser.executeUpdate();
             System.out.println("User added!");
         } catch (SQLException e) {
@@ -51,24 +51,28 @@ public class UserDAO {
     }
 
     /**
-     *  Get a list of all registered users.
+     * Get a list of all registered users.
+     *
      * @return List of AdminUser or RegisteredUser objects
      */
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         String query = "SELECT id, name, username, admin_rights FROM users;";
         List<User> listOfUsers = new ArrayList<>();
-        try{
+        try {
             Statement getUsernames = conn.createStatement();
             ResultSet rs = getUsernames.executeQuery(query);
             while(rs.next()){
                 if(rs.getBoolean(4)){
                     listOfUsers.add(new AdminUser(rs.getInt(1), rs.getString(2),rs.getString(3)));
+            while (rs.next()) {
+                if (rs.getBoolean(4)) {
+                    listOfUsers.add(new AdminUser(rs.getInt(1), rs.getString(2), rs.getString(3)));
                 }
-                if (!rs.getBoolean(4)){
-                    listOfUsers.add(new RegisteredUser(rs.getInt(1), rs.getString(2),rs.getString(3)));
+                if (!rs.getBoolean(4)) {
+                    listOfUsers.add(new RegisteredUser(rs.getInt(1), rs.getString(2), rs.getString(3)));
                 }
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Failed to get all users!");
             e.printStackTrace();
         }
@@ -94,10 +98,11 @@ public class UserDAO {
 
     /**
      * Use to get a user object from the database. Intended use case is setting the current user when logging in.
+     *
      * @param username Provide username to find in database.
      * @return AdminUser or RegisteredUser object
      */
-    public User getUser(String username){
+    public User getUser(String username) {
         String query = "SELECT * FROM users WHERE username = ?";
         User user = null;
         boolean isAdmin;
@@ -105,7 +110,7 @@ public class UserDAO {
             PreparedStatement getUser = conn.prepareStatement(query);
             getUser.setString(1, username);
             ResultSet rs = getUser.executeQuery();
-            if(rs.next()) {
+            if (rs.next()) {
                 isAdmin = rs.getBoolean(6);
                 if (isAdmin) {
                     user = new AdminUser(
@@ -128,6 +133,5 @@ public class UserDAO {
         }
         return user;
     }
-
 
 }
